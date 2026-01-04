90c9d6f2c28fefedb7cbd543e745595147503603
Oussama Taoussi <taoussi.lord@gmail.com>
2026-01-04 16:56:59 +0100

    First_Module

---
+++ ex1/ft_plot_area.py
@@ -0,0 +1,10 @@
+def ft_plot_area():
+    length = int(input("Enter length: "))
+    width = int(input("Enter width: "))
+    
+    print(f"Plot area: {length * width}")
+    
+def main():
+    ft_plot_area()
+    
+main()
