30a3e7dcf68184f4c48a09322808354638b2153f
Oussama Taoussi <outaouss@e1r5p14.1337.ma>
2026-02-18 16:54:07 +0000

    python_05

--- python_05/ex1/data_stream.py
+++ python_05/ex1/data_stream.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 
 class DataStream(ABC):
     def __init__(self, stream_id: str) -> None:
-        pass
+        self.stream_id = stream_id
 
     @abstractmethod
     def process_batch(self, data_batch: List[Any]) -> str:
@@ -13,20 +13,50 @@ class DataStream(ABC):
     def filter_data(self, data_batch: List[Any],
                     criteria: Optional[str] = None
                     ) -> List[Any]:
-        pass
+        if not criteria:
+            return data_batch
+        return [item for item in data_batch if
+                criteria.lower() in str(item).lower()]
 
     def get_stats(self) -> Dict[str, Union[str, int, float]]:
-        pass
+        stream_type = "System Events"
+        if "Sensor" in type(self).__name__:
+            stream_type = "Environmental Data"
+        elif "Transaction" in type(self).__name__:
+            stream_type = "Financial Data"
+
+        return {
+            "id": self.stream_id,
+            "type": stream_type
+        }
 
 
-class StreamPocessor:
+class StreamProcessor:
     def __init__(self):
         pass
 
     def run_unified_processing(self,
                                streams: List[DataStream],
-                               mixed_data: Dict[str, List[Any]]):
-        pass
+                               mixed_data: Dict[str, List[Any]]) -> None:
+        print("Batch 1 Results:")
+
+        for stream in streams:
+            data = mixed_data.get(stream.stream_id)
+            count = len(data)
+
+            if "Sensor" in type(stream).__name__:
+                label = "readings"
+                name = "Sensor"
+            elif "Transaction" in type(stream).__name__:
+                label = "operations"
+                name = "Transaction"
+            elif "Event" in type(stream).__name__:
+                label = "events"
+                name = "Event"
+            else:
+                print("Not Found !")
+
+            print(f"- {name} data: {count} {label} processed")
 
 
 class SensorStream(DataStream):
@@ -83,4 +113,80 @@ class EventStream(DataStream):
         super().__init__(stream_id)
 
     def process_batch(self, data_batch: List[Any]) -> str:
-        pass
+        if not data_batch:
+            return "No Data To Process"
+
+        try:
+            count = len(data_batch)
+            detect = 0
+            for check in data_batch:
+                if type(check) is not str:
+                    return "Error: The Data is Not A Text"
+                if "error" in check.lower():
+                    detect += 1
+            return f"Event analysis: {count} events, {detect} error detected"
+        except Exception:
+            return "Error: While Parsing The Data !"
+
+
+def stream_system() -> None:
+    print("=== CODE NEXUS - POLYMORPHIC STREAM SYSTEM ===\n")
+
+    mixed_data = {
+        "SENSOR_001": ["temp:22.5", "humidity:65", "pressure:1013"],
+        "TRANS_001": ["buy:100", "sell:150", "buy:75"],
+        "EVENT_001": ["login", "error", "logout"]
+    }
+
+    # Sensor Stream
+    print("Initializing Sensor Stream...")
+    data_sensor = mixed_data['SENSOR_001']
+    sensor = SensorStream("SENSOR_001")
+    print(f"Stream ID: {sensor.get_stats()['id']}, "
+          f"Type: {sensor.get_stats()['type']}")
+    print("Processing sensor batch:", f"[{', '.join(data_sensor)}]")
+    print(sensor.process_batch(data_sensor))
+
+    # Transaction Stream
+    print("\nInitializing Transaction Stream...")
+    data_transaction = mixed_data['TRANS_001']
+    transaction = TransactionStream("TRANS_001")
+    print(f"Stream ID: {transaction.get_stats()['id']}, "
+          f"Type: {transaction.get_stats()['type']}")
+    print("Processing Transaction batch:", f"[{', '.join(data_transaction)}]")
+    print(transaction.process_batch(data_transaction))
+
+    # Event Stream
+    print("\nInitializing Event Stream...")
+    data_event = mixed_data['EVENT_001']
+    event = EventStream("EVENT_001")
+    print(f"Stream ID: {event.get_stats()['id']}, "
+          f"Type: {event.get_stats()['type']}")
+    print("Processing event batch:", f"[{', '.join(data_event)}]")
+    print(event.process_batch(data_event))
+
+    # Stream Processing
+    stream_processor = StreamProcessor()
+    streams = [sensor, transaction, event]
+    print("\n=== Polymorphic Stream Processing ===")
+    print("Processing mixed stream types through unified interface...\n")
+    stream_processor.run_unified_processing(streams, mixed_data)
+
+    # filtering
+    print("\nStream filtering active: High-priority data only")
+    sensor_alerts = ["temp:50:critical", "pressure:high", "temp:55:critical"]
+
+    critical_alerts = sensor.filter_data(sensor_alerts, "critical")
+    large = transaction.filter_data(data_transaction, "150")
+
+    print(f"Filtered results: {len(critical_alerts)} "
+          f"critical sensor alerts, {len(large)} large transaction")
+
+    print("\nAll streams processed successfully. Nexus throughput optimal.")
+
+
+if __name__ == "__main__":
+    try:
+        stream_system()
+    except Exception:
+        print("Error: While Executing The 'strem_system' Function")
