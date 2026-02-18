85347a7292640fb47d4263f24dd216085d3042c6
Oussama Taoussi <outaouss@e1r11p4.1337.ma>
2026-02-18 00:30:03 +0000

    python_05

--- python_05/ex0/stream_processor.py
+++ python_05/ex0/stream_processor.py
@@ -27,10 +27,10 @@ class NumericProcessor(DataProcessor):
                 count = len(data)
                 sum_num = sum(data)
             average = sum_num / count
-            return self.format_output(f"Processed {count} numeric vlues, "
+            return self.format_output(f"Processed {count} numeric values, "
                                       f"sum={sum_num}, avg={average}")
         else:
-            return "The Provided Data Cannot Be Processed"
+            return "The Provided Data is Not Numeric"
 
     def validate(self, data: Any) -> bool:
         try:
@@ -49,10 +49,10 @@ class TextProcessor(DataProcessor):
         if self.validate(data):
             count = len(data)
             words = len(data.split())
-            return self.format_output(f"Processed test: {count} "
-                                      f"charachters, {words} words")
+            return self.format_output(f"Processed text: {count} "
+                                      f"characters, {words} words")
         else:
-            return "The Provided Data Cannot Be Processed"
+            return "The Provided Data is Not A Text"
 
     def validate(self, data: Any) -> bool:
         if isinstance(data, str):
@@ -63,27 +63,51 @@ class TextProcessor(DataProcessor):
 class LogProcessor(DataProcessor):
 
     def process(self, data: Any) -> str:
-        pass
+        if self.validate(data):
+            splited_data = data.split(':')
+            try:
+                first = splited_data[0]
+                seconde = splited_data[1]
+                return self.format_output(f"{first} level detected: {seconde}")
+            except IndexError:
+                print("Error: IndexError Raised !")
+        else:
+            return "The Provided Data Cannot be A Log Text"
 
     def validate(self, data: Any) -> bool:
-        pass
+        if isinstance(data, str):
+            splited_log = data.split()
+            short = splited_log[0]
+            if "error:" in short.lower() or "info:" in short.lower():
+                if splited_log[1] == "":
+                    return False
+                return True
+        return False
+
+    def format_output(self, result: str) -> str:
+        if "error" in result.lower():
+            return f"Output: [ALERT] {result}"
+        elif "info" in result.lower():
+            return f"Output: [INFO] {result}"
+        return super().format_output(result)
 
 
 if __name__ == "__main__":
 
     print("=== CODE NEXUS - DATA PROCESSOR FOUNDATION ===\n")
+
     # Numeric Process !
     print("Initializing Numeric Processor...")
     try:
-        data = [1, 2, 3, 4, 5]
+        data_num = [1, 2, 3, 4, 5]
     except NameError:
         print("Error: NameError Raised !")
         exit()
     numbers = NumericProcessor()
-    if numbers.validate(data):
-        print("Processing data:", data)
+    if numbers.validate(data_num):
+        print("Processing data:", data_num)
         print("Validation: Numeric data verified")
-    print(numbers.process(data), "\n")
+    print(numbers.process(data_num), "\n")
 
     # Text Process !
     print("Initializing Text Processor...")
@@ -92,11 +116,34 @@ if __name__ == "__main__":
     except NameError:
         print("Error: NameError Raised !")
         exit()
-    charachters = TextProcessor()
-    if charachters.validate(data_str):
+    characters = TextProcessor()
+    if characters.validate(data_str):
         print(f'Processing data: "{data_str}"')
         print("Validation: Text data verified")
-    print(charachters.process(data_str), "\n")
+    print(characters.process(data_str), "\n")
 
     # Log Process !
     print("Initializing Log Processor...")
+    try:
+        data_log = "ERROR: Connection timeout"
+    except NameError:
+        print("Error: NameError Raised !")
+        exit()
+    log = LogProcessor()
+    if log.validate(data_log):
+        print(f'Processing data: "{data_log}"')
+        print("Validation: Log entry verified")
+    print(log.process(data_log), "\n")
+
+    print("=== Polymorphic Processing Demo ===\n")
+    print("Processing multiple data types through same interface...")
+
+    # Demonstrate All
+    num_format = NumericProcessor()
+    print(num_format.process([2, 2, 2]))
+    text_format = TextProcessor()
+    print(text_format.process("Oussama 1337"))
+    log_format = LogProcessor()
+    print(log.process("INFO: System ready"))
+
+    print("\nFoundation systems online. Nexus ready for advanced streams.")
