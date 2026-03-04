5d1726c9f3017607cf3992aa31cdcdd8aadf7367
Oussama Taoussi <outaouss@e1r5p14.1337.ma>
2026-03-04 02:16:55 +0000

    Python_09

---
+++ Python_09/ex0/space_station.py
@@ -0,0 +1,72 @@
+from pydantic import BaseModel, Field, ValidationError
+from datetime import datetime
+from typing import Optional
+
+
+class SpaceStation(BaseModel):
+    station_id: str = Field(min_length=3, max_length=10)
+
+    name: str = Field(min_length=1, max_length=50)
+
+    crew_size: int = Field(ge=1, le=20)
+
+    power_level: float = Field(ge=0.0, le=100.0)
+
+    oxygen_level: float = Field(ge=0.0, le=100.0)
+
+    last_maintenance: datetime
+
+    is_operational: bool = True
+
+    notes: Optional[str] = Field(None, max_length=200)
+
+
+def main():
+    print("Space Station Data Validation")
+    print("========================================")
+
+    try:
+        space_valid = SpaceStation(
+            station_id="ISS001",
+            name="International Space Station",
+            crew_size=6,
+            power_level=85.5,
+            oxygen_level=92.3,
+            last_maintenance=datetime.now()
+        )
+
+        print("Valid station created:")
+        print(f"ID: {space_valid.station_id}")
+        print(f"Name: {space_valid.name}")
+        print(f"Crew: {space_valid.crew_size} people")
+        print(f"Power: {space_valid.power_level}%")
+        print(f"Oxygen: {space_valid.oxygen_level}%")
+        print("Status: "
+              f"{'Operational' if space_valid.is_operational else 'Down'}")
+
+    except ValidationError as e:
+        print(f"Unexpected error in valid data: {e}")
+
+    print("\n========================================")
+
+    print("Expected validation error:")
+    try:
+
+        space_invalid = SpaceStation(
+            station_id="MARS-01",
+            name="Overcrowded Mars Base",
+            crew_size=50,
+            power_level=50.0,
+            oxygen_level=50.0,
+            last_maintenance=datetime.now()
+        )
+
+        space_invalid.name
+
+    except ValidationError as e:
+        for error in e.errors():
+            print(f"{error['msg']}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ Python_09/ex1/alien_contact.py
@@ -0,0 +1,96 @@
+from enum import Enum
+from pydantic import model_validator, BaseModel, Field, ValidationError
+from datetime import datetime
+from typing import Optional
+
+
+class ContactType(Enum):
+    radio = "radio"
+    visual = "visual"
+    physical = "physical"
+    telepathic = "telepathic"
+
+
+class AlienContact(BaseModel):
+    contact_id: str = Field(min_length=5, max_length=15)
+
+    timestamp: datetime
+
+    location: str = Field(min_length=3, max_length=100)
+
+    contact_type: ContactType
+
+    signal_strength: float = Field(ge=0.0, le=10.0)
+
+    duration_minutes: int = Field(ge=1, le=1440)
+
+    witness_count: int = Field(ge=1, le=100)
+
+    message_received: Optional[str] = Field(None, max_length=500)
+
+    is_verified: bool = False
+
+    @model_validator(mode='after')
+    def validation_custom(self):
+        if not self.contact_id[0] == 'A' and not self.contact_id[1] == 'C':
+            raise ValueError("The Contact ID Must Start With 'AC'")
+        if not self.is_verified:
+            raise ValueError(f"The Verification is {self.is_verified}")
+        if self.witness_count < 3:
+            raise ValueError("Telepathic contact requires "
+                             "at least 3 witnesses")
+        if self.signal_strength <= 7.0:
+            raise ValueError("Recieved Signal is Weak !")
+        return self
+
+
+def main():
+    print("Alien Contact Log Validation")
+    print("======================================")
+    try:
+        alien_contact = AlienContact(contact_id="AC_2024_001",
+                                     contact_type="radio",
+                                     location="Area 51, Nevada",
+                                     signal_strength=8.5,
+                                     duration_minutes=45, witness_count=5,
+                                     timestamp=datetime.now(),
+                                     is_verified=True,
+                                     message_received="'Greetings from "
+                                     "Zeta Reticuli'")
+
+        print("Valid contact report:")
+        print("ID:", alien_contact.contact_id)
+        print("Type:", alien_contact.contact_type)
+        print("Location:", alien_contact.location)
+        print(f"Signal: {alien_contact.signal_strength}/10")
+        print(f"Duration: {alien_contact.duration_minutes} minutes")
+        print("Witnesses:", alien_contact.witness_count)
+        print("Message:", alien_contact.message_received)
+
+    except ValidationError as e:
+        print(f"Unexpected error in valid data: {e}")
+
+    print("\n======================================")
+    print("Expected validation error:")
+    try:
+        invalid_contact = AlienContact(contact_id="AC_2024_001",
+                                       contact_type="radio",
+                                       location="Area 51, Nevada",
+                                       signal_strength=8.5,
+                                       duration_minutes=45, witness_count=1,
+                                       timestamp=datetime.now(),
+                                       is_verified=True,
+                                       message_received="'Greetings from "
+                                       "Zeta Reticuli'")
+        invalid_contact.contact_id
+    except ValidationError as e:
+        for error in e.errors():
+            if 'Value error' in error['msg']:
+                valid = error['msg'].split(',')
+                print(valid[1].strip())
+            else:
+                print(error['msg'])
+
+
+if __name__ == "__main__":
+    main()
